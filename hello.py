2d0c0179d4af0b8c35ddbf54d24c0d85e3453d27
Kelvin <kelvinshu@outlook.com>
2023-10-14 12:29:23 -0600

    initial commit

---
+++ hello.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    context ={
+            'para1':'This is <strong> BOLD </strong> text',
+            'my_list':['computerScience','Language','Chemistry','Automation'],
+    }
+
+    return render_template('index.html',context=context)
+
+@app.route('/<name>')
+def user(name):
+    context={
+        'name':name,
+    }
+    return render_template("user.html",context=context)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
