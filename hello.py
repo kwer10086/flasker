972e6ad615dd6717076dec58fb595d349b3a8e73
Kelvin <kelvinshu@outlook.com>
2023-10-14 14:08:19 -0600

    add 500.html page

--- hello.py
+++ hello.py
@@ -21,3 +21,7 @@ def user(name):
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'),500
